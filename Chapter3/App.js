9290c9448fd0788de01a974b36eb93ede825c3c2
Harshal Mutyalwar <harshalmutyalwar@gmail.com>
2025-02-19 22:23:22 +0530

    React Code villa

--- Chapter3/App.js
+++ Chapter3/App.js
@@ -1,27 +1,128 @@
 import React from "react";	
-//import ReactDOM from "react-dom"  // before it is work but for new version following statement use.
 import ReactDOM from "react-dom/client";
    
 
-    //above heading using JSX
-    const heading = (
-         <h1 id="title" key="h1">
-            Namaste React
-            </h1>
+
+    const Title = () => (
+         <a href= "/">
+         <img
+         className="logo"
+         alt = "logo"
+         src = "https://lh3.googleusercontent.com/7kfhqCOmv17p7RDVZulmcFmKYiCmF8gYAEGRDOt3mfK8rwH1skFf8n_m3bGmLbnvWAiWVsIFE8Pk2i2ui_dvnpTZI9QSm4ylXQGRVPAjbQ=w1200-rw"
+         />
+         </a>
     );
 
-    // React component
-    // functional component
-    const HeaderComponent =  () => {
+    const Header =  () => {
        return (
-        <div>
-            {heading}
-            <h1>Namste React functional Component</h1>
-             <h2>This is a H2 tag</h2>
-             </div>
+        <div className = "header">
+            <Title/>
+            <div className = "nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+        );
+    };
+
+
+    const restaurantList = [{
+        name: "Burger King",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb-qzol9i3_jBlvmuTrYK0LhMMqe08oFPLmg&s",
+        cusines:["Burger", "American"],
+        rating: "4.2"
+    },
+    {
+        name: "Burger King",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb-qzol9i3_jBlvmuTrYK0LhMMqe08oFPLmg&s",
+        cusines:["Burger", "American"],
+        rating: "4.2"
+    },
+
+    {
+        name: "Burger King",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb-qzol9i3_jBlvmuTrYK0LhMMqe08oFPLmg&s",
+        cusines:["Burger", "American"],
+        rating: "4.2"
+    },
+
+    {
+        name: "Burger King",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb-qzol9i3_jBlvmuTrYK0LhMMqe08oFPLmg&s",
+        cusines:["Burger", "American"],
+        rating: "4.2"
+    },
+
+    {
+        name: "Burger King",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb-qzol9i3_jBlvmuTrYK0LhMMqe08oFPLmg&s",
+        cusines:["Burger", "American"],
+        rating: "4.2"
+    },
+
+
+    {
+        name: "Burger King",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb-qzol9i3_jBlvmuTrYK0LhMMqe08oFPLmg&s",
+        cusines:["Burger", "American"],
+        rating: "4.2"
+    }
+];
+
+    const RestrauntCard = () =>
+    {
+        return (
+        <div className="card">
+            <img src = {burgerKing.image}/>
+            <h2>{burgerKing.name}</h2>
+            <h3>{burgerKing.cusines.join(", ")}</h3>
+            <h4>{burgerKing.rating} stars</h4>
+        </div>
         );
     }
 
+    const Body = () =>
+    {
+        return(
+            <div className="restaurant-list">
+                <RestrauntCard/>
+                <RestrauntCard/>
+                <RestrauntCard/>
+                <RestrauntCard/>
+                <RestrauntCard/>
+                <RestrauntCard/>
+                <RestrauntCard/>
+                <RestrauntCard/>
+                <RestrauntCard/>
+            </div>
+        );
+    };
+
+
+    const Footer = () =>
+    {
+        return(
+            <h4>Footer</h4>
+        );
+    }
+
+    const AppLayout = () => {
+        return (
+            <>
+            <Header/>
+            <Body/>
+            <Footer/>
+            </>
+
+        )
+    }
+
+
+
     const root = ReactDOM.createRoot(document.getElementById("root")); 
 
-    root.render(<HeaderComponent/>);
+    root.render(<AppLayout/>);
